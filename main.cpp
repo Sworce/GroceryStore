e8792e5a9f7a9a41091e2c3aebe6bbd731d3fd54
Sworce <Sworce>
2019-10-28 16:41:26 -0700

    added functionality to delete last item from the list

--- main.cpp
+++ main.cpp
@@ -9,25 +9,38 @@ int main()
   vector <string> list; //vector of strings
   int numItems = 0;
   char input;
+  string itemName;
 
   do {
 
     cout<<"\n==GROCERY LIST MANAGER==";
     cout<<"\nEnter your choice: ";
     cout<<"\n (A)dd an item";
+   if(!list.empty()) {
+    cout << "\n (D)elete last item";
+    }
+
     cout<<"\n (Q)uit";
     cout<<"\nYour choice (A/Q): ";
+
     cin>>input;
 
     if(input == 'A' || input == 'a') {
-       string itemName;
        cout << "What is the item?" << endl;
        cin >> itemName;
-    
        list.push_back(itemName);
     }
+
+    if(!list.empty()) {
+         if(input == 'D' || input == 'd') {
+             cout << list.at(list.size() - 1) << 
+             " was deleted from the list." << endl;
+            list.pop_back();
+        }   
+    }
+
   }while(!(input == 'Q' || input == 'q'));
-  
+
   if((input == 'Q' || input == 'q') && (list.size() != 0)) {
     cout << "==ITEMS TO BUY==" << endl;
   
